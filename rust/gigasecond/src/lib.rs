14dd88c397d573f59c3ac43da70543dbdfa94794
stimpie007 <stimpie007@github.com>
2024-09-04 20:15:57 +1200

    Added functionality to after()

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -2,5 +2,6 @@ use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    let gigasecond = Duration::seconds(1_000_000_000);
+    return start + gigasecond
 }
