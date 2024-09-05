7d70fd4140939fb8fb1487fadbda442ae4e636f4
stimpie007 <stimpie007@github.com>
2024-09-05 12:52:43 +1200

    Added Display and new functions

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,13 +1,29 @@
-use std::fmt::Display;
+use std::fmt;
 
-pub struct Clock;
+pub struct Clock {
+    hours: i32,
+    minutes: i32
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Clock { hours, minutes }
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+    // pub fn add_minutes(&self, minutes: i32) -> Self {
+    //     // todo!("Add {minutes} minutes to existing Clock time");
+    // }
+
+    // todo!("utilise .to_string() directily");
+    // default fn to_string(&self) -> String {
+    //
+    // }
+}
+
+// Using Display
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Here we define how the struct should be formatted as a string
+        write!(f, "{}, {}", self.hours, self.minutes)
     }
 }
