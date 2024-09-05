d2f01ed5751b69235f73eb3afe9be0a26574a85c
stimpie007 <stimpie007@github.com>
2024-09-05 13:03:52 +1200

    Added add_minute

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,29 +1,23 @@
 use std::fmt;
 
 pub struct Clock {
-    hours: i32,
-    minutes: i32
+    hour: i32,
+    minute: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock { hours, minutes }
+        Clock { hour: hours, minute: minutes }
     }
 
-    // pub fn add_minutes(&self, minutes: i32) -> Self {
-    //     // todo!("Add {minutes} minutes to existing Clock time");
-    // }
-
-    // todo!("utilise .to_string() directily");
-    // default fn to_string(&self) -> String {
-    //
-    // }
+    pub fn add_minutes(&mut self, minutes: i32) {
+        self.minute += minutes
+    }
 }
 
-// Using Display
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         // Here we define how the struct should be formatted as a string
-        write!(f, "{}, {}", self.hours, self.minutes)
+        write!(f, "{}:{}", self.hour, self.minute)
     }
 }
