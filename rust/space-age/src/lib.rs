1934c46af6d13a91640250d71541973e27aa6b0d
stimpie007 <stimpie007@github.com>
2024-09-14 18:09:54 +1200

    Added functionality on planets

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,19 +1,24 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
 
-#[derive(Debug)]
-pub struct Duration;
+const EARTH_YEAR_IN_SECONDS: f64 = 31_557_600.0; // Seconds in one Earth year
 
-impl From<u64> for Duration {
-    fn from(s: u64) -> Self {
-        todo!("s, measured in seconds: {s}")
+#[derive(Debug)]
+pub struct Duration {
+    seconds: i32
+}
+
+impl Duration {
+    fn from(seconds: i32) -> Self {
+        Duration { seconds }
+    }
+     fn in_seconds(&self) -> u64 {
+        self.seconds
     }
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
-    }
+    fn years_during(duration: &Duration) -> f64;
 }
 
 pub struct Mercury;
@@ -25,11 +30,51 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+
+impl Planet for Mercury {
+    fn years_during(duration: &Duration) -> f64 {
+        duration.seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 0.2408467)
+    }
+}
+
+impl Planet for Venus {
+    fn years_during(duration: &Duration) -> f64 {
+        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 0.61519726)
+    }
+}
+
+impl Planet for Earth {
+    fn years_during(duration: &Duration) -> f64 {
+        duration.in_seconds() as f64 / EARTH_YEAR_IN_SECONDS
+    }
+}
+
+impl Planet for Mars {
+    fn years_during(duration: &Duration) -> f64 {
+        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 1.8808158)
+    }
+}
+
+impl Planet for Jupiter {
+    fn years_during(duration: &Duration) -> f64 {
+        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 11.862615)
+    }
+}
+
+impl Planet for Saturn {
+    fn years_during(duration: &Duration) -> f64 {
+        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 29.447498)
+    }
+}
+
+impl Planet for Uranus {
+    fn years_during(duration: &Duration) -> f64 {
+        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 84.016846)
+    }
+}
+
+impl Planet for Neptune {
+    fn years_during(duration: &Duration) -> f64 {
+        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 164.79132)
+    }
+}
