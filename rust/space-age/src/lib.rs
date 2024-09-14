b429df6c205d665394505493a7c0a9aeca4488d8
stimpie007 <stimpie007@github.com>
2024-09-14 18:15:13 +1200

    Changed solution

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,24 +1,17 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
+pub struct Duration { seconds: u64 }
 
-const EARTH_YEAR_IN_SECONDS: f64 = 31_557_600.0; // Seconds in one Earth year
-
-#[derive(Debug)]
-pub struct Duration {
-    seconds: i32
-}
-
-impl Duration {
-    fn from(seconds: i32) -> Self {
-        Duration { seconds }
-    }
-     fn in_seconds(&self) -> u64 {
-        self.seconds
+impl From<u64> for Duration {
+    fn from(s: u64) -> Self {
+        Duration { seconds: s }
     }
 }
 
 pub trait Planet {
-    fn years_during(duration: &Duration) -> f64;
+    fn years_during(d: &Duration) -> f64 {
+        d.seconds as f64 / Self::period()
+    }
+
+    fn period() -> f64;
 }
 
 pub struct Mercury;
@@ -30,51 +23,13 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
+const EARTH_PERIOD : f64 = 31557600.0;
 
-impl Planet for Mercury {
-    fn years_during(duration: &Duration) -> f64 {
-        duration.seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 0.2408467)
-    }
-}
-
-impl Planet for Venus {
-    fn years_during(duration: &Duration) -> f64 {
-        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 0.61519726)
-    }
-}
-
-impl Planet for Earth {
-    fn years_during(duration: &Duration) -> f64 {
-        duration.in_seconds() as f64 / EARTH_YEAR_IN_SECONDS
-    }
-}
-
-impl Planet for Mars {
-    fn years_during(duration: &Duration) -> f64 {
-        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 1.8808158)
-    }
-}
-
-impl Planet for Jupiter {
-    fn years_during(duration: &Duration) -> f64 {
-        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 11.862615)
-    }
-}
-
-impl Planet for Saturn {
-    fn years_during(duration: &Duration) -> f64 {
-        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 29.447498)
-    }
-}
-
-impl Planet for Uranus {
-    fn years_during(duration: &Duration) -> f64 {
-        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 84.016846)
-    }
-}
-
-impl Planet for Neptune {
-    fn years_during(duration: &Duration) -> f64 {
-        duration.in_seconds() as f64 / (EARTH_YEAR_IN_SECONDS * 164.79132)
-    }
-}
+impl Planet for Mercury { fn period() -> f64 { EARTH_PERIOD * 0.2408467 } }
+impl Planet for Venus   { fn period() -> f64 { EARTH_PERIOD * 0.61519726 } }
+impl Planet for Earth   { fn period() -> f64 { EARTH_PERIOD } }
+impl Planet for Mars    { fn period() -> f64 { EARTH_PERIOD * 1.8808158 } }
+impl Planet for Jupiter { fn period() -> f64 { EARTH_PERIOD * 11.862615 } }
+impl Planet for Saturn  { fn period() -> f64 { EARTH_PERIOD * 29.447498 } }
+impl Planet for Uranus  { fn period() -> f64 { EARTH_PERIOD * 84.016846 } }
+impl Planet for Neptune { fn period() -> f64 { EARTH_PERIOD * 164.79132 } }
