d4953b9bfc97b663ba069e665ab066e29b751f33
stimpie007 <stimpie007@github.com>
2024-09-16 17:41:55 +1200

    Added armstrong-numbers solution

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    let digits: Vec<u32> = num
+        .to_string() // Convert the number to a string
+        .chars() // Get its characters (digits)
+        .map(|c| c.to_digit(10).unwrap()) // Convert chars to digits
+        .collect();
+
+    let num_digits = digits.len() as u32;
+
+    let sum_of_powers: u32 = digits.iter().map(|&digit| digit.pow(num_digits)).sum();
+
+    sum_of_powers == num
 }
