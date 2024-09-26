5c80f5ee81fe20170e35f6dffef8b8b87dd33af7
stimpie007 <stimpie007@github.com>
2024-09-27 11:54:13 +1200

    Added nth solution

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,22 +1,14 @@
-pub fn nth(n: usize) -> Option<u32> {
-    // Call the list_primes function with a sufficiently high number
-    let primes = list_primes(n); // 100 is enough to include the first 25 primes
-
-}
-fn list_primes(max: u32) -> Vec<u32> {
-    if max < 2 {
-        return vec![]; // No primes less than 2
-    }
-
-    let mut primes = vec![2];
-
-    // Iterate only through odd numbers starting from 3
-    for n in (3..=max).step_by(2) {
-        let is_prime = primes.iter().take_while(|&&p| p * p <= n).all(|&p| n % p != 0);
-        if is_prime {
-            primes.push(n);
-        }
-    }
-
-    primes
+pub fn nth(n: u32) -> u32 {
+    let mut primes: Vec<u32> = vec![];
+    (2..)
+        .filter(|candidate: &u32| {
+            if !primes.iter().any(|i| candidate % i == 0) {
+                primes.push(*candidate);
+                true
+            } else {
+                false
+            }
+        })
+        .nth(n as usize)
+        .unwrap()
 }
