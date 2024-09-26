8e4d542247791596fb4df5e9ec3c60cc4b2e0e36
stimpie007 <stimpie007@github.com>
2024-09-27 11:57:59 +1200

    Reverted back to previous solution due to test case errors.

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn nth(n: u32) -> Option<u32> {
+pub fn nth(n: u32) -> u32 {
     let mut primes: Vec<u32> = vec![];
-    let mut candidate = 2;
-
-    while primes.len() <= n as usize {
-        // Check if candidate is prime
-        if primes.iter().all(|&p| candidate % p != 0) {
-            primes.push(candidate);
-        }
-        candidate += 1;
-    }
-
-    primes.last().copied() // Return the last prime found as an Option
+    (2..)
+        .filter(|candidate: &u32| {
+            if !primes.iter().any(|i| candidate % i == 0) {
+                primes.push(*candidate);
+                true
+            } else {
+                false
+            }
+        })
+        .nth(n as usize)
+        .unwrap()
 }
