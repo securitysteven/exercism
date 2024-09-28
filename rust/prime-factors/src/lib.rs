9981a3044aa17129011f9543201540c64874c0e4
stimpie007 <stimpie007@github.com>
2024-09-28 17:08:41 +1200

    Added prime-factors solution

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+    let factors: Vec<u64> = Vec::new();
+    let numerator: n;
+    let denominator = 2;
+
+    while numerator != 1 {
+        if is_whole_number(numerator, denominator) {
+            factors.push(denominator);
+        } else {
+            denominator += 1;
+        }
+    }
+}
+
+fn is_whole_number(numerator: i32, denominator: i32) -> bool {
+    numerator % denominator == 0
 }
