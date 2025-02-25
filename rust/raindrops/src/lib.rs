487c516495ad382185188912f681283d97bc6864
stimpie007 <stimpie007@github.com>
2025-02-25 15:29:03 +1300

    added: basic if-else scenario given from the README.md file

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,19 @@
+
 pub fn raindrops(n: u32) -> String {
-    todo!("what sound does Raindrop #{n} make?")
+    //is divisible by 3, add "Pling" to the result.
+    if n % 3 == 0 || n % 5 == 0 {
+        format!("Pling{}", n)
+    }
+    // is divisible by 5, add "Plang" to the result.
+    else if n % 7 == 0 {
+        format!("Plang{}", n)
+    }
+    // is divisible by 7, add "Plong" to the result.
+    else if n % 3 == 0 {
+        format!("Plong{}", n)
+    }
+    // is not divisible by 3, 5, or 7, the result should be the number as a string.
+    else {
+        n.to_string()
+    }
 }
