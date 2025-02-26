46e2c7efaa337a35edec2f2bcc13e799f6b4d4fd
stimpie007 <stimpie007@github.com>
2025-02-26 16:35:38 +1300

    feat: proverbs

---
+++ rust/proverb/src/lib.rs
@@ -0,0 +1,11 @@
+// For want of a nail the shoe was lost.
+// For want of a shoe the horse was lost.
+// For want of a horse the rider was lost.
+// For want of a rider the message was lost.
+// For want of a message the battle was lost.
+// For want of a battle the kingdom was lost.
+// And all for the want of a nail.
+
+pub fn build_proverb(list: &[&str]) -> String {
+    todo!("build a proverb from this list of items: {list:?}")
+}
