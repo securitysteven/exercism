bb5cf7d75e6ba562b16cf5d1c9f83d4694ec78b3
stimpie007 <stimpie007@github.com>
2025-03-09 10:56:35 +1300

    feat: added high_scores with solution

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+pub struct HighScores {
+    scores: Vec<u32>,
+}
+
+impl HighScores {
+    pub fn new(scores: &[u32]) -> Self {
+        Self {
+            scores: scores.to_vec(),
+        }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        &self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> { self.scores.last().copied() }
+
+    pub fn personal_best(&self) -> Option<u32> { self.scores.iter().max().copied() }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut top_scores: Vec<u32> = self.scores.clone();
+        top_scores.sort_unstable_by(|a, b| b.cmp(a));
+        top_scores.truncate(3);
+        top_scores
+    }
+}
