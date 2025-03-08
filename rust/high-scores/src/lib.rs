98e6c844c811ddaab36901dd8a77016aafdf2df5
stimpie007 <stimpie007@github.com>
2025-03-09 11:06:13 +1300

    feat: using 'a lifetime annotations.

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,27 +1,31 @@
 #[derive(Debug)]
-pub struct HighScores {
-    scores: Vec<u32>,
+pub struct HighScores<'a> {
+    scores: &'a [u32],
 }
 
-impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        Self {
-            scores: scores.to_vec(),
-        }
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
     }
 
     pub fn scores(&self) -> &[u32] {
-        &self.scores
+        self.scores
     }
 
-    pub fn latest(&self) -> Option<u32> { self.scores.last().copied() }
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().cloned()
+    }
 
-    pub fn personal_best(&self) -> Option<u32> { self.scores.iter().max().copied() }
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().cloned()
+    }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        let mut top_scores: Vec<u32> = self.scores.to_vec();
-        top_scores.sort_unstable_by(|a, b| b.cmp(a));
-        top_scores.truncate(3);
-        top_scores
+        let mut res_vec = self.scores.to_vec();
+
+        res_vec.sort_unstable_by(|a, b| b.cmp(a));
+        res_vec.truncate(3);
+
+        res_vec
     }
 }
